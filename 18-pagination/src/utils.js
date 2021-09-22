94a09ddaff983cc39dc8190c6b2c3472da76b08d
Pramod Bhoi <mail.pramod.bhoi@gmail.com>
2021-09-22 23:27:46 +0530

    pagination

---
+++ 18-pagination/src/utils.js
@@ -0,0 +1,15 @@
+const paginate = (followers) => {
+    //console.log(followers);
+    const itemsPerPage = 11;
+    const pages = Math.ceil(followers.length / itemsPerPage)
+    //console.log(pages);
+
+    const newFollowers = Array.from({length:pages},(_,index) => {
+        const start = index * itemsPerPage;
+        return followers.slice(start, start + itemsPerPage)
+    })
+    //console.log(newFollowers);
+    return newFollowers;
+}
+
+export default paginate
